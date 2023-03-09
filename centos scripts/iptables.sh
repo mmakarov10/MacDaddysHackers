86a442fde1ded0da332f746fb037cb7ebbbc720f
mmakarov10 <91093217+mmakarov10@users.noreply.github.com>
2023-03-09 09:28:49 -0600

    Create iptables.sh

---
+++ centos scripts/iptables.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Flush Existing Rules 
+iptables -F
+iptables -X
+
+#Allow for already established connections
+iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+
+#Allow ntpstat
+iptables -A INPUT -p udp -m udp -j ACCEPT
+
+# Allow Incoming Web Trafic (HTTP and HTTPS)
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+
+# Allow SSH traffic on port 64435
+iptables -A INPUT -p tcp --dport 64435 -j ACCEPT
+
+# Allow Incoming ICMP Traffic 
+iptables -A INPUT -p icmp -j ACCEPT
+
+# Allow Incoming DNS Traffic 
+iptables -A INPUT -p udp --dport 53 -j ACCEPT
+
+# Drop All Incoming Traffic 
+iptables -A INPUT -j DROP
