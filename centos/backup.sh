1f32f83c54bd8966473632f6838977f671bf9d4c
mmakarov10 <makarovmax01@gmail.com>
2023-03-10 14:01:57 -0600

    Added Files

---
+++ centos/backup.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# Check for root
+if [ $(whoami) != "root" ]; then
+    echo "Script must be run as root"
+    exit 1
+fi
+
+# Formatted output
+becho() {
+    echo "$(tput bold)$1...$(tput sgr0)"
+}
+
+# Create backups directory
+mkdir -p /var/backups
+
+becho "Backing up pam"
+cp -ra /etc/pam* /var/backups
+cp -ra /lib/security* /var/backups
+
+becho "Backing up configuration files from /etc"
+cp -ra /etc /var/backups
+
+if [ -d "/var/www" ]; then
+    becho "Backing up web files"
+    cp -r /var/www /var/backups
+fi
+
+if [ -d "/var/lib/mysql" ]; then
+    becho "Backing up MySQL"
+    mysqldump -u root -p --all-databases > /var/backups/mysql.sql
+fi
