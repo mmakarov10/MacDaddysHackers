798a53c69b2286896c803a699ce11400248fdcdf
CallSignTater <93746681+CallSignTater@users.noreply.github.com>
2023-03-09 19:45:04 -0600

    Delete AD scripts/DeepBlueCLI directory

--- AD scripts/DeepBlueCLI/safelists/readme.md
+++
@@ -1,1 +0,0 @@
-Placeholder for safelists directory
--- AD scripts/DeepBlueCLI/t/runall.ps1
+++
@@ -1,2 +0,0 @@
-Write-Host "Running DeepBlue.ps1 on all EVTX files to identify any syntax errors."
-cd .. ; gci -path . -recurse -name "*.evtx" | % {.\DeepBlue.ps1 -File $_ | Out-Null }
