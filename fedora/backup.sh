21e6bb9964dcd29d4aa54a151de11087ad315ac0
edbratkov <60157459+edbratkov@users.noreply.github.com>
2023-03-11 09:44:08 -0600

    Create backup.sh

---
+++ fedora/backup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir /etc/backups/
+mkdir /etc/backups/dovecot
+mkdir /etc/backups/dovecot/conf.d
+
+cp -ra /etc/dovecot/* /etc/backups/dovecot
+cp -ra /etc/dovecot/conf.d/* /etc/backups/dovecot/conf.d
+
+
+mkdir /etc/backups/postfix
+cp -ra /etc/postfix/* /etc/backups/postfix
+
+mkdir /etc/backups/etc
+cp -ra /etc/* /etc/backups/
