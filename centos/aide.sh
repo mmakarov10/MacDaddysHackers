61cf3db9c56a710767ccf4e752f8f9362e41d7fd
mmakarov10 <91093217+mmakarov10@users.noreply.github.com>
2023-03-11 00:33:40 -0600

    Update aide.sh

--- centos/aide.sh
+++ centos/aide.sh
@@ -3,43 +3,18 @@
 # Install AIDE
 yum install -y aide
 
-# Modify the AIDE configuration file
-cat <<EOF > /etc/aide.conf
-# AIDE configuration file
-
-database=file:/var/lib/aide/aide.db.gz
-database_out=file:/var/lib/aide/aide.db.gz.new
-gzip_dbout=yes
-verbose=4
-
-# Directories and files to monitor
-/boot R
-/bin R
-/sbin R
-/usr/bin R
-/usr/sbin R
-/usr/lib{,64} R
-/etc R
-/var/log R
-/var/spool/cron R
-/root R
-/home R
-
-# Add checks for web files and the /tmp directory
-!/var/www/html/* U
-!/var/www/cgi-bin/* U
-!/tmp/* U
-
-EOF
+# Modify the AIDE configuration file to add rules to monitor /var/www and /tmp
+sed -i '105i\/var\/www\tCONTENT' /etc/aide.conf
+sed -i '106i\/tmp\tCONTENT' /etc/aide.conf
 
 # Initialize the AIDE database with the new configuration
 aide --init
 
-# Wait for aide --init to finish
-sleep 5s
-
 # Remove the .new extension from the new database file
-mv /var/lib/aide/aide.db.gz.new /var/lib/aide/aide.db.gz
+mv /var/lib/aide/aide.db.new /var/lib/aide/aide.db.gz
+
+# Wait for AIDE database initialization to complete
+sleep 10
 
 # Create a cron job to run aide --check every 2 minutes
 echo "*/2 * * * * /usr/sbin/aide --check" | crontab -
