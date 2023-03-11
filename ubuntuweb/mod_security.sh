35a4e4300a9ceab9bb7d44cc6155b63db4103035
mmakarov10 <91093217+mmakarov10@users.noreply.github.com>
2023-03-11 10:41:59 -0600

    Update mod_security.sh

--- ubuntuweb/mod_security.sh
+++ ubuntuweb/mod_security.sh
@@ -1,38 +1,26 @@
 #!/bin/bash
 
-# Install dependencies
-apt-get install -y gcc make libxml2 libxml2-dev apache2-dev libpcre3-dev libcurl4-openssl-dev
+# Install ModSecurity and enable the module
+sudo apt update
+sudo apt install libapache2-mod-security2 -y
+sudo a2enmod security2
 
-# Install Mod_Security
-apt-get install -y libapache-mod-security2
+# Rename and configure the ModSecurity configuration file
+sudo mv /etc/modsecurity/modsecurity.conf-recommended /etc/modsecurity/modsecurity.conf
+sudo sed -i 's/SecRuleEngine DetectionOnly/SecRuleEngine On/g' /etc/modsecurity/modsecurity.conf
+sudo sed -i 's/SecAuditLogParts ABDEFHIJZ/SecAuditLogParts ABCEFHJKZ/g' /etc/modsecurity/modsecurity.conf
+
+# Download and install the OWASP Core Rule Set (CRS)
+sudo apt install -y git
+cd /tmp
+git clone https://github.com/coreruleset/coreruleset.git
+sudo mv /tmp/coreruleset/rules /etc/modsecurity/
+sudo mv /tmp/coreruleset/crs-setup.conf /etc/modsecurity/
+sudo rm -rf /tmp/coreruleset
+sudo sed -i 's#IncludeOptional /usr/share/modsecurity-crs/*.load#Include /etc/modsecurity/crs-setup.conf\nIncludeOptional /etc/modsecurity/rules/*.conf#g' /etc/apache2/mods-enabled/security2.conf
 
 # Restart Apache
-systemctl restart apache2
-
-# Move owasp-modsecurity-crs.zip to /etc/apache2/ directory and extract files
-mv /MacDaddysHackers/ubuntuweb/owasp-modsecurity-crs.zip /etc/apache2/
-unzip /etc/apache2/owasp-modsecurity-crs.zip -d /etc/apache2/
-mv /etc/apache2/owasp-modsecurity-crs /etc/apache2/modsecurity-crs
-mv /etc/apache2/modsecurity-crs/crs-setup.conf.example /etc/apache2/modsecurity-crs/crs-setup.conf
-
-# Configure ModSecurity
-cat <<EOF >> /etc/apache2/mods-enabled/security2.conf
-<IfModule security2_module>
-Include /etc/apache2/modsecurity-crs/crs-setup.conf
-Include /etc/apache2/modsecurity-crs/rules/*.conf
-</IfModule>
-EOF
-
-# Configure ModSecurity whitelist
-cat <<EOF >> /etc/apache2/modsecurity.d/whitelist.conf
-# Whitelist file to control ModSec
-<IfModule mod_security2.c>
-SecRuleEngine On
-SecRequestBodyAccess On
-SecResponseBodyAccess On
-SecDataDir /tmp
-</IfModule>
-EOF
+sudo systemctl restart apache2
 
 # Restart Apache
 systemctl restart apache2
