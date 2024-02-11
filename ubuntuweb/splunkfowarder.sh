599ca03c466591268efbd3253fe26eb27ec3d7a0
DJDetskii <112144756+DJDetskii@users.noreply.github.com>
2024-02-10 21:53:07 -0600

    Update splunkfowarder.sh

--- ubuntuweb/splunkfowarder.sh
+++ ubuntuweb/splunkfowarder.sh
@@ -1,24 +1,13 @@
-wget -O splunkforwarder-8.2.3-cd0848707637-Linux-x86_64.tgz 'https://download.splunk.com/products/universalforwarder/releases/8.2.3/linux/splunkforwarder-8.2.3-cd0848707637-Linux-x86_64.tgz'
-sudo tar xvzf splunkforwarder-8.2.3-cd0848707637-Linux-x86_64.tgz -C /opt
+wget -O splunkforwarder-9.1.1-64e843ea36b1-Linux-x86_64.tgz https://download.splunk.com/products/universalforwarder/releases/9.1.1/linux/splunkforwarder-9.1.1-64e843ea36b1-Linux-x86_64.tgz
+sudo tar xvzf splunkforwarder-9.1.1-64e843ea36b1-Linux-x86_64.tgz -C /opt
 cd /opt/splunkforwarder/bin
 sudo ./splunk start --accept-license 
 sudo ./splunk enable boot-start
 /opt/splunkforwarder/bin/splunk add forward-server 172.20.241.20:9997
-/opt/splunkforwarder/bin/splunk add monitor /var/log/auth.log -index main -sourcetype linux_secure
-/opt/splunkforwarder/bin/splunk add monitor /var/log/httpd/access_log -index main -sourcetype weblog
-/opt/splunkforwarder/bin/splunk add monitor /var/log/httpd/error_log -index main -sourcetype weblog
-/opt/splunkforwarder/bin/splunk add monitor /var/log/httpd/ssl_access_log -index main -sourcetype weblog
-/opt/splunkforwarder/bin/splunk add monitor /var/log/httpd/ssl_error_log -index main -sourcetype weblog
-/opt/splunkforwarder/bin/splunk add monitor /var/log/httpd/ssl_request_log -index main -sourcetype weblog
-/opt/splunkforwarder/bin/splunk add monitor /var/log/httpd/modsec_debug.log -index main -sourcetype weblog
-/opt/splunkforwarder/bin/splunk add monitor /var/log/httpd/modsec_audit.log -index main -sourcetype weblog
-/opt/splunkforwarder/bin/splunk add monitor /var/log/secure -index main -sourcetype syslog
-/opt/splunkforwarder/bin/splunk add monitor /var/log/httpd-errors.log -index main -sourcetype syslog
-/opt/splunkforwarder/bin/splunk add monitor /var/log/mysql.log -index main -sourcetype syslog
-/opt/splunkforwarder/bin/splunk add monitor /var/log/mysql.err -index main -sourcetype syslog
-/opt/splunkforwarder/bin/splunk add monitor /var/log/dpkg.log -index main -sourcetype syslog
-/opt/splunkforwarder/bin/splunk add monitor /var/log/syslog -index main -sourcetype syslog
-/opt/splunkforwarder/bin/splunk add monitor /var/log/authlog -index main -sourcetype syslog
-/opt/splunkforwarder/bin/splunk add monitor /var/adm/sulog -index main -sourcetype syslog
+/opt/splunkforwarder/bin/splunk add monitor /var/log/auth.log -index main -sourcetype auth -host ubuwork
+/opt/splunkforwarder/bin/splunk add monitor /var/log/syslog -index main -sourcetype syslog -host ubuwork
+/opt/splunkforwarder/bin/splunk add monitor /var/log/ufw.log -index main -sourcetype ufw -host ubuwork
+/opt/splunkforwarder/bin/splunk add monitor /var/log/syslog1 -index main -sourcetype syslog -host ubuwork
+/opt/splunkforwarder/bin/splunk add monitor /var/log/dpkg.log -index main -sourcetype dpkg.log -host ubuwork
 /opt/splunkforwarder/bin/splunk list forward-server
 /opt/splunkforwarder/bin/splunk restart
