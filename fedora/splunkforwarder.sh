7bef993d2ea74f173fb6c8c7dcaa5f3f98e1eba6
DJDetskii <112144756+DJDetskii@users.noreply.github.com>
2024-02-10 21:41:40 -0600

    Update splunkforwarder.sh

--- fedora/splunkforwarder.sh
+++ fedora/splunkforwarder.sh
@@ -1,22 +1,24 @@
-wget -O splunkforwarder-8.2.3-cd0848707637-Linux-x86_64.tgz 'https://download.splunk.com/products/universalforwarder/releases/8.2.3/linux/splunkforwarder-8.2.3-cd0848707637-Linux-x86_64.tgz'
-sudo tar xvzf splunkforwarder-8.2.3-cd0848707637-Linux-x86_64.tgz -C /opt
+#!/bin/bash
+wget -O splunkforwarder-9.1.1-64e843ea36b1-Linux-x86_64.tgz https://download.splunk.com/products/universalforwarder/releases/9.1.1/linux/splunkforwarder-9.1.1-64e843ea36b1-Linux-x86_64.tgz
+sudo tar xvzf splunkforwarder-9.1.1-64e843ea36b1-Linux-x86_64.tgz -C /opt
 cd /opt/splunkforwarder/bin
 sudo ./splunk start --accept-license 
 sudo ./splunk enable boot-start
 /opt/splunkforwarder/bin/splunk add forward-server 172.20.241.20:9997
-/opt/splunkforwarder/bin/splunk add monitor /var/log/auth.log -index main -sourcetype syslog
-/opt/splunkforwarder/bin/splunk add monitor /var/log/apache2/access.log -index main -sourcetype weblog
-/opt/splunkforwarder/bin/splunk add monitor /var/log/apache2/error.log -index main -sourcetype weblog
-/opt/splunkforwarder/bin/splunk add monitor /var/log/maillog -index main -sourcetype weblog
-/opt/splunkforwarder/bin/splunk add monitor /var/log/httpd/error_log -index main -sourcetype weblog
-/opt/splunkforwarder/bin/splunk add monitor /var/log/httpd/access_log -index main -sourcetype weblog
-/opt/splunkforwarder/bin/splunk add monitor /var/log/secure -index main -sourcetype syslog
-/opt/splunkforwarder/bin/splunk add monitor /var/log/httpd-errors.log -index main -sourcetype syslog
-/opt/splunkforwarder/bin/splunk add monitor /var/log/yum.log -index main -sourcetype syslog
-/opt/splunkforwarder/bin/splunk add monitor /var/log/mysql.err -index main -sourcetype syslog
-/opt/splunkforwarder/bin/splunk add monitor /var/log/mariadb/mariadb.log -index main -sourcetype syslog
-/opt/splunkforwarder/bin/splunk add monitor /var/log/syslog -index main -sourcetype syslog
-/opt/splunkforwarder/bin/splunk add monitor /var/log/boot.log -index main -sourcetype syslog
-/opt/splunkforwarder/bin/splunk add monitor /var/adm/sulog -index main -sourcetype syslog
+
+# This line added for centos
+
+/opt/splunkforwarder/bin/splunk add monitor /var/log/secure -index main -sourcetype secure -host fedora
+/opt/splunkforwarder/bin/splunk add monitor /var/log/messages -index main -sourcetype messages -host fedora
+/opt/splunkforwarder/bin/splunk add monitor /var/log/boot.log -index main -sourcetype boot -host fedora
+/opt/splunkforwarder/bin/splunk add monitor /var/log/cron -index main -sourcetype cron -host fedora
+/opt/splunkforwarder/bin/splunk add monitor /var/log/audit/audit.log -index main -sourcetype audit -host fedora
+/opt/splunkforwarder/bin/splunk add monitor /var/log/lastlog -index main -sourcetype lastlog -host fedora
+/opt/splunkforwarder/bin/splunk add monitor /var/log/maillog -index main -sourcetype maillog -host fedora
+/opt/splunkforwarder/bin/splunk add monitor /var/log/chrony -index main -sourcetype chrony -host fedora
+
+
+
+
 /opt/splunkforwarder/bin/splunk list forward-server
 /opt/splunkforwarder/bin/splunk restart
