e2bc6cbf716ba9dfbe59c72054fe5aa26fe28c52
Daniel Gaitan <mrdaniomi36@gmail.com>
2024-03-04 15:22:31 -0600

    Fixed starup.sh in centOS

--- centos/starup.sh
+++ centos/starup.sh
@@ -1,47 +1,23 @@
 #!/bin/bash
 
-# Function to delete user and home directory
-delete_user_and_home() {
-    username=$1
-    if [[ $username != "root" && $username != "sysadmin" ]]; then
-        userdel -r $username
-        echo "User $username and home directory deleted."
-    else
-        echo "User $username is a system user. Skipping deletion."
-    fi
-}
-
-# Function to delete all cron jobs
-delete_cron_jobs() {
-    crontab -r
-    echo "All cron jobs deleted."
-}
-
-# Function to disable SSH
-disable_ssh() {
-    systemctl stop sshd
-    systemctl disable sshd
-    echo "SSH disabled."
-}
-
-# Main script
-
-# Check if script is run as root
 if [[ $EUID -ne 0 ]]; then
     echo "This script must be run as root."
     exit 1
 fi
 
-# Delete non-sysadmin and non-root users
-while IFS=: read -r username _; do
-    delete_user_and_home $username
-done < /etc/passwd
+# Delete unauthorized users 
+awk -F: '$1 != "root" && $1 != "sysadmin" && $7 == "/bin/bash" {print $1}' /etc/passwd | xargs -I {} userdel {}
+echo "Removed all unauthorized users"
 
 # Delete all cron jobs
-delete_cron_jobs
+crontab -r
+echo "All cron jobs deleted."
 
 # Disable SSH
-disable_ssh
+systemctl stop sshd
+systemctl disable sshd
+sudo yum remove openssh-server
+echo "SSH disabled."
 
 echo "Script execution completed."
 
