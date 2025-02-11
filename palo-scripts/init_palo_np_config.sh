5a3912fd06281c1134df7c611b12f1837a8ab543
mmakarov10 <makarovmax01@gmail.com>
2025-02-11 13:30:42 -0600

    Update name change

--- palo-scripts/init_palo_np_config.sh
+++ palo-scripts/init_palo_np_config.sh
@@ -30,6 +30,10 @@ set service "service-ssh" protocol tcp port 22
 
 delete rulebase security
 set rulebase security rules "Allow Ping" from any source any to any destination any application ping service application-default action allow
+set rulebase security rules "Allow HTTPS to Palo MGT from WKS" from User source "WKS" to "User" destination "Palo Mgmt" application any service service-https action allow
+set rulebase security rules "Allow SSH to Palo MGT from WKS" from User source "WKS" to "User" destination "Palo Mgmt" application "ssh" service "service-ssh" action allow
+set rulebase security rules "Block Egress Traffic from WKS" from User source "WKS" to any destination any application any service any action deny
+set rulebase security rules "Block Egress Traffic from WKS" disabled yes
 set rulebase security rules "Ubuntu Web External Access" from External source any to User destination "Ubuntu Web-NAT" application web-browsing service application-default action allow
 set rulebase security rules "Ubuntu Web External Access" disabled yes
 set rulebase security rules "AD DNS External Access" from External source any to User destination "AD-NAT" application dns service application-default action allow
@@ -37,19 +41,15 @@ set rulebase security rules "Centos E-comm External Access" from External source
 set rulebase security rules "Fedora Mail SMTP External Access" from External source any to Public destination "Fedora-NAT" application smtp service application-default action allow
 set rulebase security rules "Fedora Mail POP3 External Access" from External source any to Public destination "Fedora-NAT" application pop3 service application-default action allow
 set rulebase security rules "Fedora Mail IMAP External Access" from External source any to Public destination "Fedora-NAT" application imap service application-default action allow 
+set rulebase security rules "Splunk Web Access Exteranl Access" from External source any to Public destination "Splunk-NAT" application web-browsing service Port-8000 action allow
+set rulebase security rules "Debian DNS External Access" from External source any to Internal destination "Debian-NAT" application dns service application-default action allow
 set rulebase security rules "Fedora AD Auth" from Public source Fedora to User destination AD application ldap service application-default action allow 
 set rulebase security rules "Internal to Splunk App" from Internal source any to Public destination "Splunk" application splunk service application-default action allow
 set rulebase security rules "User to Splunk App" from User source any to Public destination "Splunk" application splunk service application-default action allow
 set rulebase security rules "Internal Serve Splunk" from Internal source any to Public destination "Splunk" application web-browsing service Port-8000 action allow
 set rulebase security rules "User Serve Splunk" from User source any to Public destination "Splunk" application web-browsing service Port-8000 action allow
-set rulebase security rules "External Web Access to Splunk" from External source any to Public destination "Splunk-NAT" application web-browsing service Port-8000 action allow
-set rulebase security rules "External DNS Access" from External source any to Internal destination "Debian-NAT" application dns service application-default action allow
 set rulebase security rules "Public NTP Access to Debian" from Public source any to Internal destination "Debian" application ntp service application-default action allow
-set rulebase security rules "User NTP Access" from User source any to Internal destination "Debian" application ntp service application-default action allow
-set rulebase security rules "Allow HTTPS to Palo MGT from WKS" from User source "WKS" to "User" destination "Palo Mgmt" application any service service-https action allow
-set rulebase security rules "Allow SSH to Palo MGT from WKS" from User source "WKS" to "User" destination "Palo Mgmt" application "ssh" service "service-ssh" action allow
-set rulebase security rules "Block Egress Traffic from WKS" from User source "WKS" to any destination any application any service any action deny
-set rulebase security rules "Block Egress Traffic from WKS" disabled yes
+set rulebase security rules "User NTP Access to Debian" from User source any to Internal destination "Debian" application ntp service application-default action allow
 set rulebase security rules "Egress Allow DNS from Debian" from Internal source Debian to External destination any application dns service application-default action allow
 set rulebase security rules "Egress Allow DNS from Debian" disabled yes
 set rulebase security rules "Egress Allow NTP from Debian" from Internal source Debian to External destination any application ntp service application-default action allow
@@ -70,10 +70,10 @@ set rulebase security rules "Internal to Splunk App" log-start yes log-end yes
 set rulebase security rules "User to Splunk App" log-start yes log-end yes
 set rulebase security rules "Internal Serve Splunk" log-start yes log-end yes
 set rulebase security rules "User Serve Splunk" log-start yes log-end yes
-set rulebase security rules "External Web Access to Splunk" log-start yes log-end yes
-set rulebase security rules "External DNS Access" log-start yes log-end yes
+set rulebase security rules "Splunk Web Access Exteranl Access" log-start yes log-end yes
+set rulebase security rules "Debian DNS External Access" log-start yes log-end yes
 set rulebase security rules "Public NTP Access to Debian" log-start yes log-end yes
-set rulebase security rules "User NTP Access" log-start yes log-end yes
+set rulebase security rules "User NTP Access to Debian" log-start yes log-end yes
 set rulebase security rules "Allow HTTPS to Palo MGT from WKS" log-start yes log-end yes
 set rulebase security rules "Allow SSH to Palo MGT from WKS" log-start yes log-end yes
 set rulebase security rules "Block Egress Traffic from WKS" log-start yes log-end yes
@@ -145,10 +145,10 @@ set rulebase security rules "Internal to Splunk App" profile-setting group Best_
 set rulebase security rules "User to Splunk App" profile-setting group Best_Practice_Security
 set rulebase security rules "Internal Serve Splunk" profile-setting group Best_Practice_Security
 set rulebase security rules "User Serve Splunk" profile-setting group Best_Practice_Security
-set rulebase security rules "External Web Access to Splunk" profile-setting group Best_Practice_Security
-set rulebase security rules "External DNS Access" profile-setting group Best_Practice_Security
+set rulebase security rules "Splunk Web Access Exteranl Access" profile-setting group Best_Practice_Security
+set rulebase security rules "Debian DNS External Access" profile-setting group Best_Practice_Security
 set rulebase security rules "Public NTP Access to Debian" profile-setting group Best_Practice_Security
-set rulebase security rules "User NTP Access" profile-setting group Best_Practice_Security
+set rulebase security rules "User NTP Access to Debian" profile-setting group Best_Practice_Security
 set rulebase security rules "Block Egress Traffic from WKS" profile-setting group Best_Practice_Security
 set rulebase security rules "Allow HTTPS to Palo MGT from WKS" profile-setting group Best_Practice_Security
 set rulebase security rules "Allow SSH to Palo MGT from WKS" profile-setting group Best_Practice_Security
